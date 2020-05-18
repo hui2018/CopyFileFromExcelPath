0b65162bb9d4632a1114524df990bf3c7929a42a
Jack <hui2@pdx.edu>
2020-05-17 20:07:35 -0700

    run hemo tool in cmd

--- readFile.py
+++ readFile.py
@@ -1,14 +1,19 @@
 import pandas as pd
 import shutil as st
+import runHemoTool as hemo
 
+def main():
+    df = pd.read_excel('C:\\Users\\Jack\\Desktop\\test\\test.xlsx', sheet_name='Sheet2')
+    mylist = df['A'].tolist()
 
-df = pd.read_excel('C:\\Users\\Jack\\Desktop\\test\\test.xlsx', sheet_name='Sheet2')
-mylist = df['A'].tolist()
+    for a in range(len(mylist)):
+        splitting = mylist[a].split('\\')
+        fileName = splitting[len(splitting)-1]
+        st.copyfile(mylist[a], 'C:\\Users\\Jack\\Desktop\\des\\' + fileName)
 
-for a in range(len(mylist)):
-    splitting = mylist[a].split('\\')
-    fileName = splitting[len(splitting)-1]
-    st.copyfile(mylist[a], 'C:\\Users\\Jack\\Desktop\\des\\' + fileName)
+    hemo.runHemo()
 
+if __name__ == "__main__":
+    main()
 
 
